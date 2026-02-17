fcb773b0e78b842f785a387a13f9c6d69c3783a6
Bruno Proenca <bruno.proenca@synchro.com.br>
2026-02-17 18:07:13 -0300

    mcp client Filesystem

---
+++ README.md
@@ -0,0 +1,13 @@
+# Getting Started
+
+### Project Setup
+
+Configure the environment variable: `OPENAI_API_KEY` with your OpenAI API key.
+
+### Running the Application
+
+In WSL environment, run the following command to start the application:
+
+```bash
+./mvnw spring-boot:run
+```
--- src/main/java/com/github/bproenca/mcpclientbcp/controller/MCPClientController.java
+++ src/main/java/com/github/bproenca/mcpclientbcp/controller/MCPClientController.java
@@ -2,6 +2,7 @@ package com.github.bproenca.mcpclientbcp.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,8 +11,11 @@ public class MCPClientController {
 
     private  final ChatClient chatClient;
 
-    public MCPClientController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
+    public MCPClientController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider toolCallbackProvider) {
+        this.chatClient = chatClientBuilder
+                .defaultToolCallbacks(toolCallbackProvider)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
     }
 
     @GetMapping("/chat")
